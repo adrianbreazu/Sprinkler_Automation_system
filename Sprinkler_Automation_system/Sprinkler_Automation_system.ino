1f80b27f8b6becd7625c893820a6e8209e718126
Breazu Adrian <breazuadrian@gmail.com>
2015-03-09 21:41:03 +0200

    1R1HS
    test with 1 relay and 1 humidity sensor

--- Sprinkler_Automation_system/Sprinkler_Automation_system.ino
+++ Sprinkler_Automation_system/Sprinkler_Automation_system.ino
@@ -3,4 +3,32 @@
 // Copyright 2015 License: GNU GPL v3 http://www.gnu.org/licenses/gpl-3.0.html
 // ---------------------------------------------------------------------------
 
+// connect the soil sensor to Analog pin A0
+// connect the relay to digital pin D7
 
+#define Relay 7
+
+int soilHumidity;
+
+void setup () {
+	Serial.begin(9800);
+	//initialize Relay data pin
+	pinMode(Relay, OUTPUT);
+}
+
+void loop() {
+        //read soilHumidity
+        soilHumidity = analogRead(0);
+  
+	//print soil value
+	Serial.print("Soil humdity value: ");
+	Serial.println(soilHumidity);
+	delay(500);
+        if (soilHumidity > 300)
+        {
+          Serial.println("Start relay for 2 seconds");
+	  digitalWrite(Relay,LOW);
+	  delay(2000);
+  	  digitalWrite(Relay,HIGH);
+        }
+}
